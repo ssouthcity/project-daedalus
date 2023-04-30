816fb5880ed666155227bc9dc89be0944d579df4
Stian Sørby <ssouthcity@gmail.com>
2023-04-30 18:06:04 +0200

    configured bevy

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
+
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::LIME_GREEN,
+            custom_size: Some(Vec2::new(32.0, 32.0)),
+            ..default()
+        },
+        ..default()
+    });
 }
